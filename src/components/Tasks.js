d79ee4b281f2792368380e615a13adfeb252ef4b
witold-herman <56408790+witold-herman@users.noreply.github.com>
2019-11-13 19:34:27 +0100

    Styles, adding projects
    Added some styles and "Add Project" option. It doesn't work like I expected tho. Adding projects to Firebase, but not displaying them. Need to fix.

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,21 +1,42 @@
-import React from 'react';
-import {Checkbox} from './Checkbox'
-import {useTasks} from '../hooks';
+import React, { useEffect } from 'react';
+import { Checkbox } from './Checkbox';
+import { useTasks } from '../hooks';
+import { collatedTasks } from '../constants';
+import { getTitle, getCollatedTitle, collatedTasksExist } from '../helpers';
+import { useSelectedProjectValue, useProjectsValue } from '../context';
 
 export const Tasks = () => {
-    const { tasks } = useTasks('1');
+    const { selectedProject } = useSelectedProjectValue();
+    const { projects } = useProjectsValue();
+    const { tasks } = useTasks(selectedProject);
 
-    console.log(tasks);
     let projectName = '';
 
-    return (
-        <div className='tasks' data-testid='tasks'>
-            <h2 data-testid='project-name'>{projectName}</h2>
+    if (collatedTasksExist(selectedProject) && selectedProject) {
+        projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+    }
 
-            <ul className='tasks__list'>
+    if (
+        projects &&
+        projects.length > 0 &&
+        selectedProject &&
+        !collatedTasksExist(selectedProject)
+    ) {
+        projectName = getTitle(projects, selectedProject).name;
+    }
+
+    useEffect(() => {
+        document.title = `${projectName}: Todoist`;
+    });
+
+    return (
+        <div className="tasks" data-testid="tasks">
+            <h2 data-testid="project-name">{projectName}</h2>
+
+            <ul className="tasks__list">
                 {tasks.map(task => (
                     <li key={`${task.id}`}>
-                        <Checkbox id={task.id} />
+                        <Checkbox id={task.id} taskDesc={task.task} />
                         <span>{task.task}</span>
                     </li>
                 ))}
